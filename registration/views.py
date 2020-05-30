6d0418fd6d746a7bbe605f9250c46590607a0c5e
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-05-30 16:51:00 -0500

    Creando un perfil de usuario (prueba) y haciendolo editable

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,11 @@
 from .forms import UserCreationFormWithEmail
 from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
+from .models import Profile
 
 # Create your views here.
 class SignUpView(CreateView):
@@ -29,4 +33,14 @@ class SignUpView(CreateView):
             attrs={'class':'form-control mb-2', 'placeholder':'Repite la contraseña'})
         return form
 
-  
+@method_decorator(login_required, name='dispatch')
+class ProfileUpdate(UpdateView):
+    model = Profile
+    fields = ['avatar', 'bio', 'link']
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_form.html'
+
+    def get_object(self):
+        #RECUPERAR EL OBJETO QUE SE VA  A EDITAR
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        return profile
