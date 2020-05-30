848fa36f99aa5f5817279256da06459435febc81
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-05-30 13:35:54 -0500

    Creando el crud, comenzando con el create y update

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 #from . import views
-from .views import PagesListView, PageDetailView
+from .views import PagesListView, PageDetailView, PageCreate, PageUpdate
 
-urlpatterns = [
+pages_patterns = ([
     path('', PagesListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
-]
+    path('create/', PageCreate.as_view(), name='create'),
+    path('update/<int:pk>/', PageUpdate.as_view(), name='update'),
+
+], 'pages')
--- webPlayground/urls.py
+++ webPlayground/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pages.urls import pages_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('pages/', include('pages.urls')),
+    path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
 ]
