f9f28ca7bd931e39afbdd96b139b7fbde1792e7b
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-05-30 18:17:26 -0500

    Realizando pruebas unitarias y optimizando el almacenamiento del avatar

--- registration/models.py
+++ registration/models.py
@@ -4,10 +4,16 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 # Create your models here.
+def custom_upload_to(instance, filename):
+    old_instance = Profile.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return 'profiles/' + filename
+
+
 #VAMOS A CREAR UN AVATAR 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='profiles', null=True, blank=True)
+    avatar = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
 
@@ -15,7 +21,7 @@ class Profile(models.Model):
 def ensure_profile_exists(sender, instance, **kwargs):
     if kwargs.get('created', False):
         Profile.objects.get_or_create(user=instance)
-       # print("Se acaba de crear un usuario y su perfil enlazado")
+        print("Se acaba de crear un usuario y su perfil enlazado")
 
 
     
--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+#VAMOS A CREAR UNA PRUEBA
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user('test', 'test@test.es', 'test1234')
+
+    def test_profile_exists(self):
+        exists = Profile.objects.filter(user__username='test').exists()
+        self.assertEqual(exists, True)
